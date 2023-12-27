f7ceb03ffb7604564a93c4aa2295fdf01f281468
phduyanhhh <aphan1304@gmail.com>
2023-12-28 02:28:38 +0700

    duyanhInfomation

--- DuyAnhAdmin/getDataStudent.php
+++ DuyAnhAdmin/getDataStudent.php
@@ -1,3 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    require "connect.php";
+    $sqlStudent = "SELECT * FROM tai_khoan WHERE ma_vai_tro = 3";
+    $sqlStudentStudying = "SELECT * FROM hoc_sinh";
+    $resultStudent = $conn->query($sqlStudent);
+    $resultStudentStudying = $conn->query($sqlStudentStudying);
+    // ĐIỂM ĐẦU VÀO CAO
+
+
+    ?>
+    <div class="header-content-info-student">
+        <h2>Infomation Student</h2>
+        <div>
+            <div>Students account number: <?php echo $resultStudent->num_rows; ?></div>
+            <div>Number of Students registered for the course: <?php echo $resultStudentStudying->num_rows; ?></div>
+        </div>
+    </div>
+    <div class='box-information'>
+        <div class='item-box-information'>
+            <h3>Students</h3>
+            
+        </div>
+        <div class='item-box-information'>
+            <h3>Student scores</h3>
+            <div><a class="nav-link" href=""><b>Top students with the highest entrance scores</b></a></div>
+            <div><a class="nav-link" href=""><b>Top students have study points</b></a></div>
+        </div>
+        <div class='item-box-information'>
+            <h3>Course</h3>
+
+        </div>
+        <div class='item-box-information'>
+            <h3>Class</h3>
+
+        </div>
+    </div>
+</body>
+</html>
 <?php
+require 'connect.php';
 echo "hello";
 ?>
--- DuyAnhAdmin/js/ajax_information.js
+++ DuyAnhAdmin/js/ajax_information.js
@@ -1,13 +1,15 @@
-function detail_school() {
+function detail_student() {
+    // gọi phương thức XMLHttpRequest
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200){
             document.querySelector('#content').innerHTML = this.responseText;
         }
     };
-    xmlhttp.open("GET", "../getDataStudent.php", true);
+    // MỞ FILE XỬ LÍ DATA
+    xmlhttp.open("GET", "getDataStudent.php", true);
     xmlhttp.send();
 }
 document.addEventListener("DOMContentLoaded", function(){
-    document.querySelector('#details').onclick = detail_school;
+    document.querySelector('#details').onclick = detail_student;
 });
