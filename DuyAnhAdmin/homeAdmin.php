0e8c223e0073818eb17074995d16d6dea583b488
phduyanhhh <aphan1304@gmail.com>
2023-12-27 21:45:15 +0700

    duyanhCssAdmin

--- DuyAnhAdmin/homeAdmin.php
+++ DuyAnhAdmin/homeAdmin.php
@@ -6,11 +6,14 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="cssAdmin/style-home-admin.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-..." crossorigin="anonymous"/>
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary" id="nav-menu-top">
+<?php
+session_start();
+  if(isset($_SESSION['ten'])){
+    ?>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" id="nav-menu-top">
     <div class="container-fluid nav-menu">
       <a class="navbar-brand" href="#"><img src="../image/img-british-council.jpg" class='img-logo'></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,7 +27,7 @@
         </div>
       </div>
       <div class="dropdown mt-3" id="box-avt">
-        <div class="dropdown-toggle" type="button" data-bs-toggle="dropdown" id="user-avt"><img src="../image/admin.png" id="img-admin"><p class="name">Admin</p></div> 
+        <div class="dropdown-toggle" type="button" data-bs-toggle="dropdown" id="user-avt"><img src="../image/admin.png" id="img-admin"><p class="name"><?php echo $_SESSION['ten']; ?></p></div> 
         <ul class="dropdown-menu left drop-menu">
           <li><a class="dropdown-item" href="#">Action</a></li>
           <li><a class="dropdown-item" href="#">Another action</a></li>
@@ -85,11 +88,11 @@
                 </div>
                 <div class="accordion-item">
                     <h2 class="accordion-header">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                           Class
                       </button>
                     </h2>
-                    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
                       <div class="accordion-body">
                           <a class="nav-link" href=""><b>Add Class</b></a>
                           <a class="nav-link" href=""><b>Edit Class</b></a>
@@ -101,28 +104,32 @@
               </div>
         </div>
     </div>
-    <div class="item content">
+    <div class='item content'>
         <h2>Infomation</h2>
-        <div>
-            <div>
+        <div class='box-information'>
+            <div class='item-box-information'>
                 <h3>Student</h3>
                 <div>Số học sinh của trung tâm</div>
             </div>
-            <div>
+            <div class='item-box-information'>
                 <h3>Teacher</h3>
                 <div>Số học sinh của trung tâm</div>
             </div>
-            <div>
+            <div class='item-box-information'>
                 <h3>Course</h3>
                 <div>Số học sinh của trung tâm</div>
             </div>
-            <div>
+            <div class='item-box-information'>
                 <h3>Class</h3>
-                <div>Số học sinh của trung tâm</div>
+                <div>Số học sinh</div>
             </div>
         </div>
     </div>
 </content>
+    <?php
+  }
+?>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
